79b25a417046f9d313a76d582e4b4991276625a9
Satyam Bansal <bansalsatyam2504@gmail.com>
2024-03-27 13:52:54 +0530

    first commit

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,50 @@
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+from sklearn.model_selection import train_test_split   as tts
+from dataclasses import dataclass
+
+
+## intialize the data ingestion configuration
+
+@dataclass
+class DataIngestionconfig:
+    train_data_path = os.path.join('artifacts', 'train.csv')
+    
+    test_data_path= os.path.join('artifacts','test.csv' )
+    raw_data_path =os.path.join('artifacts', 'raw.csv')
+
+## create a data ingestion class
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionconfig()
+        
+    def initiate_data_ingestion(self):
+        logging.info('Data Ingestion method starts')
+        
+        try:
+            df = pd.read_csv(os.path.join('notebooks/data', 'gemstone.csv'))
+            logging.info('Dataset read as pandas Dataframe')
+            
+            
+            ## if artifacts folder do not exist
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+            
+            df.to_csv(self.ingestion_config.raw_data_path, index=False)
+            logging.info('train test split')
+            train_set, test_set = tts(df, test_size=0.30, random_state=30)
+            
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+             
+            logging.info('Ingestion of data completed')
+            
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+            
+        except Exception as e:
+            logging.info('error occured in data Ingestion config')    
---
+++ src/exception.py
@@ -0,0 +1,31 @@
+import sys
+from src.logger import logging
+
+def error_message_detail(error, error_detail):
+    _,_,exc_tb = error_detail
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+    
+    return error_message
+    
+    
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+        
+        logging.info(self.error_message)
+            
+    def __str__(self):
+        return self.error_message
+
+if __name__ == "__main__":
+    logging.info("logging has started")
+    
+    try:
+        a = 1/0
+    except Exception as e:
+        raise CustomException(str(e), sys.exc_info())
